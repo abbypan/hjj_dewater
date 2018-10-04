845578b3afc5d54b76922b1371b7df98e3f4b743
abbypan <abbypan@gmail.com>
2018-10-04 20:18:49 +0800

    div

--- hjj_dewater.js
+++ hjj_dewater.js
@@ -5,7 +5,7 @@ function banner_path() {
 }
 
 function extract_floor_info(info) {
-	var c = info.html();
+	var c = info.find("div:eq(1)").html();
     var meta = info.parents("tr:eq(1)").next().text();
 	var m = meta.match(/№(\d+).+?☆☆☆(.*?)于([\d\s:-]+)留言☆☆☆/);
 	return {
