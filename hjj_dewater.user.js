5d032b13aed6f64f5097406023c32e1fd818edff
abbypan <abbypan@gmail.com>
2019-08-11 16:21:03 +0800

    https

--- hjj_dewater.user.js
+++ hjj_dewater.user.js
@@ -2,13 +2,14 @@
 //
 // ==UserScript==
 // @name          hjj_dewater 
-// @namespace     http://abbypan.github.io/
-// @version       0.2
+// @namespace     https://github.com/abbypan
+// @version       0.3
 // @author        Abby Pan (abbypan@gmail.com)
-// @description   红晋江( http://bbs.jjwxc.net ) 贴子脱水
-// @copyright     2013, Abby Pan (http://abbypan.github.io/) 
+// @description   红晋江( bbs.jjwxc.net ) 贴子脱水
+// @copyright     2013, Abby Pan ( https://abbypan.github.io/ )
 // @grant         GM_getResourceText
-// @include       http://bbs.jjwxc.net/showmsg.php?board=*&id=*
+// @include       *://bbs.jjwxc.net/showmsg.php?board=*&id=*
+// @include       *://bbs.jjwxc.com/showmsg.php?board=*&id=*
 // @resource      hjj_dewater https://raw.githubusercontent.com/abbypan/hjj_dewater/master/hjj_dewater.js
 // @resource      bbs_dewater https://raw.githubusercontent.com/abbypan/bbs_dewater/master/bbs_dewater.js
 // 
